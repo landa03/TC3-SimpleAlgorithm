e53851ed312e543fd7ed0586d98aa95aec287c12
landa03 <60487799+landa03@users.noreply.github.com>
2020-09-18 18:30:12 -0700

    Add files via upload

---
+++ TC3-SimpleAlgorithm/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace TC3_SimpleAlgorithm
+{
+    class Program
+    {
+        static int numero;
+        static void Main(string[] args)
+        {
+           System.Console.WriteLine("escrive la lonjitud de tu array");
+           numero = Convert.ToInt32(Console.ReadLine());
+           System.Console.Write(numero);
+           for (int i = 1; i <= numero; i++)
+           {
+               System.Console.Write($",{i}");
+           }
+        }
+    }
+}
+//pare que usas numeros, cuando puedes usar letras que paresen numeros
